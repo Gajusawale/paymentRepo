acc9c11e74f8c240a49dc50571f1de10bbd52a3e
Gajusawale <95343807+Gajusawale@users.noreply.github.com>
2024-06-06 09:15:45 +0530

    Delete payment directory

--- payment/payment/src/main/java/com/payment/repositories/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.payment.repositories;
-
-import com.payment.entities.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
--- payment/payment/src/main/java/com/payment/repositories/VendorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.payment.repositories;
-
-import com.payment.entities.Vendor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VendorRepository extends JpaRepository<Vendor, Long> {
-
-   Vendor findByName(String name);
-}
